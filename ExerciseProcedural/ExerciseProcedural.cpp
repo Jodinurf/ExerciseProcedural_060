6c61479706381ae32f750b6c76d399179f43f57c
Jodinurf <jodinurfarkhani04@gmail.com>
2023-03-29 09:58:18 +0700

    membuat prosedur untuk mencari status diterima atau tidaknya menggunakan tipe data string

--- ExerciseProcedural/ExerciseProcedural.cpp
+++ ExerciseProcedural/ExerciseProcedural.cpp
@@ -5,3 +5,12 @@ double rerata(double a, double b) {
 	return (a + b) / 2;
 }
 
+string status(double r, double n) {
+	if (r >= 70 or n > 80) {
+		return "Diterima";
+	}
+	else {
+		return "Belum Diterima";
+	}
+}
+
